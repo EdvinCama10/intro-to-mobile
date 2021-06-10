7308de975f086d6bc4548088de9c3a4cbee2906e
Mahadi Babiker <mahadib124@gmail.com>
2021-06-10 23:46:29 +0200

    Validation for Registration

--- DentalApplication/app/src/main/java/com/example/dentalapplication/RegistrationActivity.java
+++ DentalApplication/app/src/main/java/com/example/dentalapplication/RegistrationActivity.java
@@ -3,9 +3,11 @@ package com.example.dentalapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -29,19 +31,31 @@ public class RegistrationActivity extends AppCompatActivity {
         enteredFirstName = findViewById(R.id.enteredFirstName);
         enteredLastName = findViewById(R.id.enteredLastname);
         enteredPhoneNumber = findViewById(R.id.enteredPhoneNumber);
-        enteredAddress =findViewById(R.id.enteredAddress);
+        enteredAddress = findViewById(R.id.enteredAddress);
         enteredPassword = findViewById(R.id.enteredPassword);
         enteredRePassword = findViewById(R.id.enteredRePassword);
         register = findViewById(R.id.register);
 
 
-register.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        registerUser();
-    }
-});
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if (checkIfFieldsAreEmpty()) {
+
+                    Toast.makeText(RegistrationActivity.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    if (checkIfPasswordsMatch()) {
+                        registerUser();
+                    } else {
+                        Toast.makeText(RegistrationActivity.this, "Passwords Don't match !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+
+        });
 
 
     }
@@ -60,8 +74,35 @@ register.setOnClickListener(new View.OnClickListener() {
 
         String method = "register";
         BackgroundTask backgroundTask = new BackgroundTask(this);
-        backgroundTask.execute(method,userName,email,firstName,lastName,phoneNumber,address,password,rePassword);
+        backgroundTask.execute(method, userName, email, firstName, lastName, phoneNumber, address, password, rePassword);
         finish();
 
     }
+
+    private boolean checkIfFieldsAreEmpty() {
+        if (TextUtils.isEmpty(enteredUserName.getText()) ||
+                TextUtils.isEmpty(enteredEmail.getText()) ||
+                TextUtils.isEmpty(enteredFirstName.getText()) ||
+                TextUtils.isEmpty(enteredLastName.getText()) ||
+                TextUtils.isEmpty(enteredPhoneNumber.getText()) ||
+                TextUtils.isEmpty(enteredAddress.getText()) ||
+                TextUtils.isEmpty(enteredPassword.getText()) ||
+                TextUtils.isEmpty(enteredRePassword.getText())) {
+
+            return true;
+        }
+
+        return false;
+
+
+    }
+
+    private boolean checkIfPasswordsMatch() {
+        if (enteredPassword.getText().toString().trim().equals(enteredRePassword.getText().toString().trim())) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
